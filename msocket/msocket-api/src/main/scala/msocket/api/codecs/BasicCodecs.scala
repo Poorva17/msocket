0880e4cd980a2b2a644efa38e80075b4ebc1fc95
Mushtaq Ahmed <mushtaq.a@gmail.com>
2019-12-18 20:28:21 +0530

    upgrade to borer 1.3

--- msocket/msocket-api/src/main/scala/msocket/api/codecs/BasicCodecs.scala
+++ msocket/msocket-api/src/main/scala/msocket/api/codecs/BasicCodecs.scala
@@ -4,7 +4,7 @@ import java.util.concurrent.TimeUnit
 
 import akka.Done
 import akka.util.Timeout
-import io.bullet.borer.derivation.ArrayBasedCodecs
+import io.bullet.borer.derivation.CompactMapBasedCodecs
 import io.bullet.borer.{Codec, Decoder, Encoder}
 import msocket.api.models.Result
 
@@ -12,11 +12,8 @@ import scala.concurrent.duration.FiniteDuration
 
 object BasicCodecs extends BasicCodecs
 trait BasicCodecs {
-  implicit def eitherEnc[E: Encoder, S: Encoder]: Encoder[Either[E, S]] =
-    implicitly[Encoder[Result[S, E]]].contramap(Result.fromEither)
-
-  implicit def eitherDec[E: Decoder, S: Decoder]: Decoder[Either[E, S]] =
-    implicitly[Decoder[Result[S, E]]].map(_.toEither)
+  implicit def eitherCodec[E: Encoder: Decoder, S: Encoder: Decoder]: Codec[Either[E, S]] =
+    Codec.of[Result[S, E]].bimap(Result.fromEither, _.toEither)
 
   implicit lazy val doneCodec: Codec[Done] = Codec.bimap[String, Done](_ => "done", _ => Done)
 
@@ -25,5 +22,5 @@ trait BasicCodecs {
     { case (length, unitStr) => FiniteDuration(length, TimeUnit.valueOf(unitStr)) }
   )
 
-  implicit lazy val timeoutCodec: Codec[Timeout] = ArrayBasedCodecs.deriveUnaryCodec
+  implicit lazy val timeoutCodec: Codec[Timeout] = CompactMapBasedCodecs.deriveCodec
 }
